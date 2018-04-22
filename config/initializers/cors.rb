10644290dfc8999a19a34a8e0e0982b9f1ebd9c6
Williams Isaac <williamscalg@gmail.com>
2018-04-22 23:00:40 +0100

    whitelisted fe heroku url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200'
+    origins 'localhost:4200','opencr.herokuapp.com'
     resource '*',
       headers: :any,
       credentials: true,
